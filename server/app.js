83691592b7a4aac5bfe0c9fd2a1a7f49abaa3fc7
mandeukJeong <wjdalstj137@gmail.com>
2024-07-09 14:48:43 +0900

    feat: cors 설정

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 // const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 const userRouter = require('./routes/user');
@@ -12,6 +13,7 @@ app.set('port', process.env.PORT);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/user', userRouter);
 
