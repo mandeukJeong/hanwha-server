a6a5f72b33779c59c92cf63e6dcc994d973ab2c4
mandeukJeong <wjdalstj137@gmail.com>
2024-10-01 01:59:05 +0900

    fix: chat socket 로직 수정

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
@@ -21,7 +22,7 @@ const { Server } = require('socket.io');
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000/',
     methods: ['GET', 'POST'],
     credentials: true,
   },
@@ -64,34 +65,45 @@ connectDB
 
 io.on('connection', (socket) => {
   socket.on('ask-join', (roomId) => {
+    console.log('채팅방에 입장하였습니다.');
     socket.join(roomId);
   });
 
   socket.on('message-send', async (data) => {
     const cookies = socket.request.headers.cookie;
-    const userId = cookieParser.JSONCookies(cookie.parse(cookies)).user;
-    const nickname = cookie.parse(cookies).nickname;
 
-    await db.collection('chatMessage').insertOne({
-      parentRoom: new ObjectId(data.room),
-      content: data.message,
-      who: new ObjectId(userId),
-      nickname,
-      date: new Date(),
-    });
+    if (cookies) {
+      const userId = cookieParser.JSONCookies(cookie.parse(cookies)).user;
+      const nickname = cookie.parse(cookies).nickname;
 
-    io.to(data.room).emit('message-broadcast', {
-      content: data.message,
-      who: userId,
-      nickname,
-    });
+      await db.collection('chatMessage').insertOne({
+        parentRoom: new ObjectId(data.room),
+        content: data.message,
+        who: new ObjectId(userId),
+        nickname,
+        date: new Date(),
+      });
+
+      io.to(data.room).emit('message-broadcast', {
+        content: data.message,
+        who: userId,
+        nickname,
+      });
+    }
   });
 
   socket.on('leave-room', (roomId) => {
+    console.log('채팅방을 떠났습니다.');
     socket.leave(roomId);
+    socket.disconnect();
   });
 });
 
 server.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
+});
+
+app.use(express.static(path.join(__dirname, './build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
